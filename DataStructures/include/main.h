bb047dd8dbb74b27881fccb30da09d4bdad0d554
RayhaanT <tanweer.rayhaan@gmail.com>
2021-01-06 14:15:07 -0500

    Reorganized functions

--- DataStructures/include/main.h
+++ DataStructures/include/main.h
@@ -1,5 +1,26 @@
 # pragma once
 
 #include <string>
+#include "customer.h"
+#include "deliverable.h"
 
+// Utility functions
 std::string stripString(std::string str);
+std::string removeExcessDecimals(std::string str);
+Customer * selectCustomer();
+Deliverable * selectOrder(Customer * customer);
+
+// Menu functions
+void addCustomer();
+void deleteCustomer();
+void printOrderStatuses();
+void addOrder();
+void cancelOrder();
+void recordPay();
+void deliver();
+void printLabel();
+void readOrder();
+
+// Globals
+extern std::vector<Customer> allCustomers;
+const Customer failedSearch("ERROR", "ERROR", "ERROR", "ERROR", "ERROR");
